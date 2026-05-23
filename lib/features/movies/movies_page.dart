d38ad9e4f6bf454fa8b82afe530aa97d51c07c63
2fbg <fabioguarniere@gmail.com>
2026-05-23 17:25:56 -0300

    Update movies_page.dart

--- lib/features/movies/movies_page.dart
+++ lib/features/movies/movies_page.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../../services/demo_iptv_library_service.dart';
 import '../shared/content_grid_page.dart';
 
 class MoviesPage extends StatelessWidget {
-  const MoviesPage({super.key});
+  const MoviesPage({super.key, required this.result});
+
+  final dynamic result;
 
   @override
   Widget build(BuildContext context) {
-    final library = DemoIptvLibraryService.build();
+    final library = result.library;
 
     return ContentGridPage(
       title: 'Filmes',
-      subtitle: 'VODs separados dos canais e das séries',
+      subtitle: 'VODs reais da playlist atual',
       items: library.movieItems,
       icon: Icons.movie_creation_outlined,
       accentColor: const Color(0xFF00A3FF),
