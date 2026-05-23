6a22372deb9a503d02afcf1637ffe3ff99be12c1
2fbg <fabioguarniere@gmail.com>
2026-05-23 17:26:24 -0300

    Update series_page.dart

--- lib/features/series/series_page.dart
+++ lib/features/series/series_page.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../../services/demo_iptv_library_service.dart';
 import '../shared/content_grid_page.dart';
 
 class SeriesPage extends StatelessWidget {
-  const SeriesPage({super.key});
+  const SeriesPage({super.key, required this.result});
+
+  final dynamic result;
 
   @override
   Widget build(BuildContext context) {
-    final library = DemoIptvLibraryService.build();
+    final library = result.library;
 
     return ContentGridPage(
       title: 'Séries',
-      subtitle: 'Episódios detectados por padrão S01E01 e 1x01',
+      subtitle: 'Episódios detectados na playlist atual',
       items: library.seriesItems,
       icon: Icons.video_library_outlined,
       accentColor: const Color(0xFF7C3AED),
